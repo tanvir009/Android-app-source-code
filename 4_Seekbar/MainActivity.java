62be9ce1698a7b806d56e08f1a35d76e9bd28c45
MD TASNIN TANVIR <73347678+tanvir009@users.noreply.github.com>
2020-10-24 16:28:40 +0600

    Add files via upload

--- 4_Seekbar/MainActivity.java
+++ 4_Seekbar/MainActivity.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.my1;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,46 +38,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-=======
-package com.example.my1;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.widget.RatingBar;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
-public class MainActivity extends AppCompatActivity {
-
-    private SeekBar seekBar;
-    private TextView textView;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        seekBar = findViewById(R.id.seekBarId);
-        textView = findViewById(R.id.textViewId);
-        textView.setText("volume : "+seekBar.getProgress()+"/"+seekBar.getMax());
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                textView.setText("volume : "+i+"/"+seekBar.getMax());
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(MainActivity.this,"onStartTrackingTouch",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(MainActivity.this,"onStopTrackingTouch",Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
->>>>>>> f1ec52e322284a58232871deadaa97b8fc0613ba
 }
